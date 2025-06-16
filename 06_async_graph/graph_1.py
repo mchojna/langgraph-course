11d6dfea8e2fa65fbe5a7d6f4723c05a40a45d66
Michał Chojna <m.chojna@icloud.com>
2025-06-16 18:01:33 +0200

    Prepared async graphs

---
+++ 06_async_graph/graph_1.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+
+import operator
+from typing import Annotated, Any
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+
+load_dotenv()
+
+class State(TypedDict):
+    aggregate: Annotated[list, operator.add]
+
+class ReturnNodeValue:
+    def __init__(self, node_secret: str):
+        self._value = node_secret
+        
+    def __call__(self, state: State) -> Any:
+        import time
+        time.sleep(1)
+        print(f"Adding {self._value} to {state['aggregate']}")
+        return {"aggregate": [self._value]}
+
+builder = StateGraph(State)
+
+builder.add_node("a", ReturnNodeValue("I'm A"))
+builder.add_node("b1", ReturnNodeValue("I'm B1"))
+builder.add_node("b2", ReturnNodeValue("I'm B2"))
+builder.add_node("c", ReturnNodeValue("I'm C"))
+builder.add_node("d", ReturnNodeValue("I'm D"))
+
+builder.add_edge(START, "a")
+builder.add_edge("a", "b1")
+builder.add_edge("a", "c")
+builder.add_edge("b1", "b2")
+builder.add_edge(["b2", "c"], "d")
+builder.add_edge("d", END)
+
+graph = builder.compile()
+graph.get_graph().draw_mermaid_png(output_file_path="06_async_graph/graph_1.png")
+
+if __name__ == "__main__":
+    graph.invoke({"aggregate": []}, config={"configurable": {"thread_id": "foo"}})
